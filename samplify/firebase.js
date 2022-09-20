def1a0d7a79778cc722479158196fa7466f474f9
Gareth McIntosh <gareth.mcintosh11@gmail.com>
2022-09-20 16:28:13 +1200

    added firebase.js config

---
+++ samplify/firebase.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDQ-PZGYrgs0kmqoFDOSiz9FtFf10WQ0fg",
+    authDomain: "samplify-4ad35.firebaseapp.com",
+    databaseURL: "https://samplify-4ad35-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "samplify-4ad35",
+    storageBucket: "samplify-4ad35.appspot.com",
+    messagingSenderId: "611779178684",
+    appId: "1:611779178684:web:2fd323298e0ba24f04e0b9",
+    measurementId: "G-DGYHP8WEEB"
+};
